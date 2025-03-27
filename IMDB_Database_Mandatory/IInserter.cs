f308d98bf2ab85e401e3a9b52842c02de13eccf9
Muhammed Tastan <muha082195@gmail.com>
2025-03-27 19:40:26 +0100

    Tilføjet IInserter. Nok den eneste Klasse som er helt færdigt xD

---
+++ IMDB_Database_Mandatory/IInserter.cs
@@ -0,0 +1,16 @@
+﻿using IMDB_Database_Mandatory.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Database_Mandatory
+{
+    public interface IInserter
+    {
+
+        void Insert(List<Title> titles, List<Person> persons, List<Genre> genres, HashSet<string> professions, SqlConnection sqlConn, SqlTransaction transAction);
+    }
+}
